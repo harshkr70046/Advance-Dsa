003c280696d4b55f39d1d2ed98094b1b68e29178
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2024-11-20 23:14:21 +0530

    changes

--- stack_queue/basic.cpp
+++ stack_queue/basic.cpp
@@ -79,6 +79,55 @@ public:
     }
 
 };
+
+
+class Impliment_queue {
+    int front;
+    int back;
+    int q[10];
+    const int capacity = 10;
+    int count;
+public:
+    Impliment_queue () : front(0),back(0),count(0){};
+    void push(int x){
+        if(count==capacity){
+            cout<<"\nqueue is full..";
+            return;
+        }
+        q[back] = x;
+        back = (back+1)%capacity;
+        count++;
+    }
+    void pop(){
+        if(count==0){
+            cout<<"\nqueue is empty..";
+            return;
+        }
+        front = (front+1)%capacity;
+        count--;
+    }
+
+    int front_(){
+        if(count==0){
+            cout<<"\nqueue is empty..";
+            return -1;
+        }
+        return q[front];
+    }
+    int back_(){
+        if(count==0){
+            cout<<"\nqueue is empty..";
+            return -1;
+        }
+        return q[(back-1+capacity)%capacity];
+    }
+    int size(){
+        return count;
+    }
+    bool empty(){
+        return count==0;
+    }
+};
 int main(){
     // int n;
     // cout<<"\nenter the size of the array = ";
@@ -88,24 +137,41 @@ int main(){
     // print_array(arr,n);
     // use_stack(arr,n);
     // use_queue(arr,n);
-    impliment_stack harsh;
+    // impliment_stack harsh;
+    // harsh.push(1);
+    // harsh.push(2);
+    // harsh.push(3);
+    // harsh.push(4);
+    // cout<<"\ntop element = "<<harsh.topElement();
+    // cout<<"\nstack size = "<<harsh.size();
+
+    // harsh.pop();
+    // cout<<"\nafter pop top element = "<<harsh.topElement();
+    // cout<<"\nafter pop stack size = "<<harsh.size();
+
+    // cout<<"\nEmpty or not = "<<harsh.isEmpty();
+    // harsh.pop();
+    // harsh.pop();
+    // harsh.pop();
+    // cout<<"\nafter pop = "<<harsh.isEmpty();
+
+    // harsh.pop();
+
+    Impliment_queue harsh;
     harsh.push(1);
     harsh.push(2);
     harsh.push(3);
     harsh.push(4);
-    cout<<"\ntop element = "<<harsh.topElement();
-    cout<<"\nstack size = "<<harsh.size();
+    harsh.push(5);
 
+    cout<<"\nbefore pop front element = "<<harsh.front_();
+    cout<<"\nbefore pop back element = "<<harsh.back_();
+    cout<<"\nsize before pop = "<<harsh.size();
     harsh.pop();
-    cout<<"\nafter pop top element = "<<harsh.topElement();
-    cout<<"\nafter pop stack size = "<<harsh.size();
+    cout<<"\nafter pop front element = "<<harsh.front_();
+    cout<<"\nafter pop back element = "<<harsh.back_();
+    cout<<"\nafter pop size = "<<harsh.size();
 
-    cout<<"\nEmpty or not = "<<harsh.isEmpty();
-    harsh.pop();
-    harsh.pop();
-    harsh.pop();
-    cout<<"\nafter pop = "<<harsh.isEmpty();
-
-    harsh.pop();
-
+    cout<<"\nis empty = "<<harsh.empty();
+    
 }
