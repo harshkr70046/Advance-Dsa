48ef652e807590f6c9bbf5198ef4bda9fdd00a63
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2024-11-23 23:00:18 +0530

    changes

--- stack_queue/problem_on_stack.cpp
+++ stack_queue/problem_on_stack.cpp
@@ -20,15 +20,109 @@ bool is_balance_parantheses(string s){
     }
     return st.size()==0;
 }
+//! infix to post_fix
 
+int priority(char c){
+    if(c=='^') return 3;
+    else if(c=='*' || c=='/') return 2;
+    else if(c=='+' || c=='-') return 1;
+    else return -1;
+}
+string infix_to_post_fix(string s){
+    stack<char>st;
+    string ans = "";
+    for(int i=0;i<s.size();i++){
+        if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z')
+        || (s[i]>='0' && s[i]<='9')){
+            ans+=s[i];
+        }
+        else if(s[i]=='('){
+            st.push('(');
+        }
+        else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                ans+=st.top();
+                st.pop();
+            }
+            if (!st.empty()) st.pop();
+        }
+        else{
+            while (!st.empty() && priority(s[i])<=priority(st.top())){
+                ans+=st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+    }
+    return ans;
+}
+
+//! infix to prifix
+
+int priority_check__(char c){
+    if(c=='^') return 3;
+    else if(c=='*' || c=='/') return 2;
+    else if(c=='+' || c=='-') return 1;
+    else return 0;
+}
+string infix_to_prifix(string s){
+    // step1 reverse the infix
+    reverse(s.begin(),s.end());
+    // Step2: fixing the bricket
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') s[i] = ')';
+        else if (s[i] == ')') s[i] = '(';
+    }
+    //step3 convert infix to post fix
+
+    stack<char>st;
+    string ans = "";
+    for(int i=0;i<s.size();i++){
+        if((s[i]>='A' && s[i]<='Z')||(s[i]>='a' && s[i]<='z')||(s[i]>='0' && s[i]<='9')){
+            ans+=s[i];
+        }
+        else if(s[i]=='(') st.push(s[i]);
+        else if(s[i]==')'){
+            while(!st.empty() && st.top()!='('){
+                ans+=st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else{
+             while (!st.empty() && priority_check__(s[i]) < priority_check__(st.top()) ||
+                   (!st.empty() && priority_check__(s[i]) == priority_check__(st.top()) && s[i] != '^')) {
+                ans += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+    }
+    // step4 reverse the ans;
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
 int main(){
     string s;
     cout<<"\nenter the string = ";
     cin>>s;
-    if(is_balance_parantheses(s)){
-        cout<<"\nyes it is balance.";
-    }
-    else{
-        cout<<"\nno it is not balance.";
-    }
+    // if(is_balance_parantheses(s)){
+    //     cout<<"\nyes it is balance.";
+    // }
+    // else{
+    //     cout<<"\nno it is not balance.";
+    // }
+    // string infixToPostFix = infix_to_post_fix(s);
+    // cout<<"\n Infix to post fix = "<<infixToPostFix;
+
+    string InfixToPrifix = infix_to_prifix(s);
+    cout<<"\n Infix to pre fix = "<<InfixToPrifix;
 }
