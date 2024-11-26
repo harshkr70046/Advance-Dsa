0d1883067e73511de7c4714c48a56fbf658002a6
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2024-11-26 23:52:06 +0530

    changes

--- stack_queue/Hard_level.cpp
+++ stack_queue/Hard_level.cpp
@@ -91,6 +91,89 @@ int total_tapped_water(vector<int>&arr,int n){
     }
     return total;
 }
+
+//! sum of subarray minimum.
+//! next smaller element
+vector<int> next_smaller_element(vector<int>&arr,int n){
+    vector<int>nse(n);
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        nse[i] = (st.empty())?n:st.top();
+        st.push(i);
+    }
+    return nse;
+}
+//! previous smaller element
+vector<int> previous_smaller_element(vector<int>&arr,int n){
+    vector<int>pse(n);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()]>arr[i]){
+            st.pop();
+        }
+        pse[i] = (st.empty())?-1:st.top();
+        st.push(i);
+    }
+    return pse;
+}
+int sum_subarray(vector<int>&arr,int n){
+    vector<int>nse = next_smaller_element(arr,n);
+    vector<int>pse = previous_smaller_element(arr,n);
+    int total = 0;
+    int mod = (int)1e9+7;
+    for(int i=0;i<n;i++){
+        int left = i-pse[i];
+        int right = nse[i]-i;
+        total = (total + 1LL * right * left * arr[i] % mod) % mod;
+    }
+    return total;
+}
+
+//! brute force
+int total_subarray_sum(vector<int>&arr,int n){
+    int total = 0;
+    int mod = (int)1e9+7;
+    for(int i=0;i<n;i++){
+        int mini = arr[i];
+        for(int j=i;j<n;j++){
+            mini = min(mini,arr[j]);
+            total = (total + mini)%mod;
+        }   
+    }
+    return total;
+}
+
+//! sum of subarray ranges it means
+//* to find the answer of this problem we need to find nse pse nge pge
+//!next smaller element
+vector<int> nSe(vector<int>&arr,int n){
+    vector<int>nse(n);
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+
+        nse[i] = (st.empty())?n:st.top();
+        st.push(i);
+    }
+    return nse;
+}
+//! previous smaller element
+vector<int> pSe(vector<int>&arr,int n){
+    vector<int>pse(n);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+
+        pse[i] = (st.empty())?-1:st.top();
+        st.push(i);
+    }
+    return pse;
+}
+//! next greater element;
+vector<int> nGe(vector<int>&arr,int n)
 int main(){
     int n;
     cout<<"\nenter the size of the array = ";
@@ -110,5 +193,9 @@ int main(){
 
     // cout<<"\ntotal tapped rain = "<<tappingTotalWater(arr,n);
 
-    cout<<"\ntotal tapped rain = "<<total_tapped_water(arr,n);
+    // cout<<"\ntotal tapped rain = "<<total_tapped_water(arr,n);
+
+    cout<<"\ntotal sum of subarray minimum = "<<sum_subarray(arr,n);
+
+    cout<<"\ntotal sum of subarray minimum = "<<total_subarray_sum(arr,n);
 }
--- stack_queue/tempCodeRunnerFile.cpp
+++ stack_queue/tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-cout<<"\ntotal tapped rain = "<<tappingTotalWater(arr,n);
+//! brute force
+int total_subarray_sum(vector<int>&arr,int n){
+    int total = 0;
+    int mod = (int)1e9+7;
+    for(int i=0;i<n;i++){
+        int mini = arr[i];
+        for(int j=i;j<n;j++)
+        mini = min(mini,arr[j]);
+        total+=mini%mod;
+    }
+    return total;
+}
