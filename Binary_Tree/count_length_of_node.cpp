bb70799cb43863f57e7986fae36f8e40087ac791
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2024-12-19 22:37:49 +0530

    changes

--- Binary_Tree/count_length_of_node.cpp
+++ Binary_Tree/count_length_of_node.cpp
@@ -42,7 +42,19 @@ int find_length_of_bt(node* root){
     if(root==nullptr) return 0;
     return (1+find_length_of_bt(root->left)+find_length_of_bt(root->right));
 }
-
+//* find the total sum of bt data
+//! first method
+int total_sum(node* root){
+    if(root==nullptr) return 0;
+    return (root->data+total_sum(root->left)+total_sum(root->right));
+}
+//! second method
+void total_sum_of_bt(node* root,int &sum){
+    if(root==nullptr) return;
+    sum+=root->data;
+    total_sum_of_bt(root->left,sum);
+    total_sum_of_bt(root->right,sum);
+}
 int main(){
     cout<<"\nenter the root node = ";
     node* root = create_binary_tree();
@@ -52,4 +64,9 @@ int main(){
     find_length_of_tree(root,cnt);
     cout<<"\nlength of tree = "<<cnt;
     cout<<"\n length using second method = "<<find_length_of_bt(root);
+
+    cout<<"\n total sum of the bt = "<<total_sum(root);
+    int sum = 0;
+    total_sum_of_bt(root,sum);
+    cout<<"\nsecond method of sum = "<<sum;
 }
