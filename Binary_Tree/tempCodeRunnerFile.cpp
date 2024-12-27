902aa8931f2a54c762266abb57374617ad6c75e3
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2024-12-27 23:12:10 +0530

    changes

--- Binary_Tree/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    vector<int> ans = inorder_itterator(root);
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
-    }
---
+++ Binary_Tree/verticalTraversalBt.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    int data;
+    node* left;
+    node* right;
+    node(int data1){
+        data = data1;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+
+int main(){
+
+}
