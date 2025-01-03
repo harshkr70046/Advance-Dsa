7cd1b55c9a11c61648c5a138bb636cf56b39f894
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2025-01-04 05:19:37 +0530

    changes

---
+++ recursion/reverseString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void solve(string &s,int ind,string &r){
+    if(ind==s.size()) return;
+    solve(s,ind+1,r);
+    r.push_back(s[ind]);
+}
+string reverse(string &s){
+    string r = "";
+    solve(s,0,r);
+    return r;
+}
+int feb(int n ){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return feb(n-1)+feb(n-2);
+}
+
+int main(){
+    string s = "Harsh";
+    string ans =  reverse(s);
+    cout<<"reverses = "<<ans;
+    // cout<<"\nfeb = "<<feb(6);
+
+    cout<<endl;
+    cout<<feb(6);
+}
