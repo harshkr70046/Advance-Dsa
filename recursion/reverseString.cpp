b24801e2475157607b7384b35db47495c664076b
Harsh kumar <168617734+harshkr70046@users.noreply.github.com>
2025-01-05 21:33:10 +0530

    changes

--- recursion/reverseString.cpp
+++ recursion/reverseString.cpp
@@ -1,28 +1,35 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void solve(string &s,int ind,string &r){
-    if(ind==s.size()) return;
-    solve(s,ind+1,r);
+void solve(string &s, int ind, string &r)
+{
+    if (ind == s.size())
+        return;
+    solve(s, ind + 1, r);
     r.push_back(s[ind]);
 }
-string reverse(string &s){
+string reverse(string &s)
+{
     string r = "";
-    solve(s,0,r);
+    solve(s, 0, r);
     return r;
 }
-int feb(int n ){
-    if(n==0) return 0;
-    if(n==1) return 1;
-    return feb(n-1)+feb(n-2);
+int feb(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return feb(n - 1) + feb(n - 2);
 }
 
-int main(){
+int main()
+{
     string s = "Harsh";
-    string ans =  reverse(s);
-    cout<<"reverses = "<<ans;
+    string ans = reverse(s);
+    cout << "reverses = " << ans;
     // cout<<"\nfeb = "<<feb(6);
 
-    cout<<endl;
-    cout<<feb(6);
+    cout << endl;
+    cout << feb(6);
 }
